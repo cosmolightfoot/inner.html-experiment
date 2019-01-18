9d62ac42ce49fba0d5be69a60dcca9e7087da650
Cosmo Kent <cosmolightfoot@gmail.com>
2019-01-17 19:18:11 -0800

    button returns even/odd

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 function getValue() {
     var y = document.getElementById('output');
     var x = document.getElementById('input').value;
-   
-
+    if(x % 2 === 0) {
+        x = 'even';
+        console.log('Even');
+        
+    }
+    else {
+        x = 'Odd';
+    }
 
     y.innerHTML = x;
 }
