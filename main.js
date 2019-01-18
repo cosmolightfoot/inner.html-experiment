cff7dec04f945c7f72e03d8b3953be1f8f4f275b
Cosmo Kent <cosmolightfoot@gmail.com>
2019-01-17 17:06:36 -0800

    successful text input field

---
+++ main.js
@@ -0,0 +1,8 @@
+function getValue() {
+    var y = document.getElementById('output');
+    var x = document.getElementById('input').value;
+    var z = document.getElementById('input2').value;
+
+
+    y.innerHTML = x + " " + z;
+}
