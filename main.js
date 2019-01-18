62b99dfccd5c1b2a830bb6281d3c53f16f10ad18
Cosmo Kent <cosmolightfoot@gmail.com>
2019-01-17 19:11:12 -0800

    changed text to even or odd

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function getValue() {
     var y = document.getElementById('output');
     var x = document.getElementById('input').value;
-    var z = document.getElementById('input2').value;
+   
 
 
-    y.innerHTML = x + " " + z;
+    y.innerHTML = x;
 }
